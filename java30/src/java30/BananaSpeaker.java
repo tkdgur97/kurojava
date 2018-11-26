8a072a642bcdd94f1f671769eddc87d884d549d2
jungwonalicia <aliciawill@naver.com>
2018-11-26 16:40:16 +0900

    Add files via upload

--- java30/src/java30/BananaSpeaker.java
+++ java30/src/java30/BananaSpeaker.java
@@ -1,6 +1,9 @@
 package java30;
 
 public class BananaSpeaker implements Speaker {
+	public void add() {
+		System.out.println("바나나 스피커 추가...");
+	}
 	public void soundUp() {
 		System.out.println("바나나 스피커 소리 UP");
 	}
--- java30/src/java30/KiwiSpeaker.java
+++ java30/src/java30/KiwiSpeaker.java
@@ -10,7 +10,10 @@ public class KiwiSpeaker implements Speaker {
 	@Override
 	public void soundDown() {
 		System.out.println("키위 스피커의 사운드 DOWN");
-
+	}
+	@Override
+	public String toString() {
+		return "나는 키위";
 	}
 
 }
